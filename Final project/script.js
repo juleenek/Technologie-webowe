d684fcad1c6f52d177cf62d96fec3c74583160fc
Julia Linek <leeenek0@gmail.com>
2021-04-30 00:26:49 +0200

    Trying to end main page on mobile devices

--- Final project/script.js
+++ Final project/script.js
@@ -4,3 +4,15 @@ function openNav(){
 function closeNav(){
     document.getElementById('side-nav').style.width="0"
 }
+function initMap() {
+    const komorowo = { lat: 51.372292, lng: 17.086189 };
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 7,
+      center: komorowo,
+    });
+    const marker = new google.maps.Marker({
+      position: komorowo,
+      map: map,
+    });
+  }
+  
