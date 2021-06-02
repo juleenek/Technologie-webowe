4a5d21592ed06c414ee7d50fc1cc6884dccb30bc
Julia Linek <leeenek0@gmail.com>
2021-06-02 14:57:08 +0200

    Changes

--- Selfy website/script.js
+++ Selfy website/script.js
@@ -9,7 +9,26 @@ function openNav() {
 
   // SCALE PHOTO
 
+  const img1 = document.querySelector('#gallery-img1');
+  const img2 = document.querySelector('#gallery-img2');
+  const img3 = document.querySelector('#gallery-img3');
+  const img4 = document.querySelector('#gallery-img4');
+  const img5 = document.querySelector('#gallery-img5');
+  
+  img1.addEventListener('click', () => openLightbox('images/blog/photo-7.jpeg'));
+  img2.addEventListener('click', () => openLightbox('images/blog/photo-1.jpeg'));
+  img3.addEventListener('click', () => openLightbox('images/blog/photo-6.jpeg'));
+  img4.addEventListener('click', () => openLightbox('images/blog/photo-4.jpeg'));
+  img5.addEventListener('click', () => openLightbox('images/blog/photo-2.jpeg'));
 
+  var imagesGallery = [img1, img2, img3, img4, img5];
+
+  function openLightbox(imgSource){
+    const lightbox = document.querySelector('.lightbox');
+    const lightboxImage = document.querySelector('#lightbox-image');
+    lightbox.classList.add('lightbox-visible');
+    lightboxImage.src = imgSource;
+  }
 
   // VIDEO SRIPT
 
