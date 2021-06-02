15258fe64ea71de1aed592894969ec31dd3b1356
Julia Linek <leeenek0@gmail.com>
2021-06-02 11:39:47 +0200

    Details

--- Selfy website/script.js
+++ Selfy website/script.js
@@ -9,26 +9,7 @@ function openNav() {
 
   // SCALE PHOTO
 
-function myFunction() {
-   var element = document.getElementById("photoScale");
-   element.classList.add("scale");
-}
-function myFunctionTwo() {
-  var element = document.getElementById("photoScale2");
-  element.classList.add("scale");
-}
-function myFunctionThree() {
-  var element = document.getElementById("photoScale3");
-  element.classList.add("scale");
-}
-function myFunctionFour() {
- var element = document.getElementById("photoScale4");
- element.classList.add("scale");
-}
-function myFunctionFive() {
-  var element = document.getElementById("photoScale5");
-  element.classList.add("scale");
-}
+
 
   // VIDEO SRIPT
 
