ee2bd374faaeb3b0c98537656179a9b0a2e38a53
Julia Linek <leeenek0@gmail.com>
2021-06-15 20:00:38 +0200

    Near to end commit

--- Final project/script.js
+++ Final project/script.js
@@ -5,13 +5,13 @@ function closeNav(){
     document.getElementById('side-nav').style.width="0"
 }
 function initMap() {
-    const komorowo = { lat: 51.372292, lng: 17.086189 };
+    const trzebnica = { lat: 51.372292, lng: 17.086189 };
     const map = new google.maps.Map(document.getElementById("map"), {
       zoom: 7,
-      center: komorowo,
+      center: trzebnica,
     });
     const marker = new google.maps.Marker({
-      position: komorowo,
+      position: trzebnica,
       map: map,
     });
   }
